bc53b1a007bbeaedccf89fad586a13fb153a0236
lalabuy948 <lalabuy9948@gmail.com>
2024-08-15 14:45:32 +0200

    chore: cleanup unused files and code

--- assets/vite.config.js
+++ assets/vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import liveVuePlugin from "live_vue/vitePlugin";
 import vuetify from "vite-plugin-vuetify";
-// import vuetify from "./plugins/vuetify";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
